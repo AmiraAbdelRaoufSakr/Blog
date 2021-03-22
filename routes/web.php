19a17a17a82f64acdde6362a047ae9515e677673
AmiraSakr-Intake41 <amira.21sakr@gmail.com>
2021-03-22 13:49:01 +0200

    Adding authentication on routes

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ Route::get('/', function () {
     return view('welcome');
   
 });
+Route::group(['middleware' => ['auth']],function(){
 Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
 Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
 Route::post('/posts',[PostController::class, 'store'])->name('posts.store');
@@ -26,6 +27,7 @@ Route::get('/posts/{post}/edit',[PostController::class, 'edit'])->name('posts.ed
 Route::put('/posts/{post}',[PostController::class, 'update'])->name('posts.update');
 Route::get('/posts/{post}',[PostController::class, 'show'])->name('posts.show');
 Route::delete('/posts/{post}',[PostController::class, 'destroy'])->name('posts.destroy');
+});
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
